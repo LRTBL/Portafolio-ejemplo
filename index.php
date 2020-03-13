782ac9c490377100e0f16c197169c6b9ca6e5776
Mahiler <fernando.mahiler@gmail.com>
2020-03-12 23:07:32 -0500

    Disenio resposive terminado

---
+++ index.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
+    <meta name='viewport' content='width=device-width, user-scalable=no'>
+    <title>Fernando Chullo</title>
+    <link rel="stylesheet" href="./css/styles.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="owl/owl.carousel.min.css" />
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css" />
+    <!-- <script src='main.js'></script> -->
+  </head>
+  <body>
+    <a id="whatsapp" href="https://api.whatsapp.com/send?phone=945373476"><img src="img/whatsapp.png" alt=""></a>
+    <header>
+      <div class="menu">
+        <div class="container">
+          <p class="logo">Fernando Chullo</p>
+          <img class="menu-icon" src="img/menu.png">
+          <nav>
+            <ul>
+              <li><a href="#about-me">Sobre mi</a></li>
+              <li><a href="#services">Servicios</a></li>
+              <li><a href="#my-jobs">Mis trabajos</a></li>
+              <li><a href="#why-works">¿Por que trabajar conmigo?</a></li>
+              <li><a href="#blog">Blog</a></li>
+              <li><a href="#contact">Contacto</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="container" id="container-title-flex">
+        <div class="container-title">
+          <h1>Fernando Mahiler Chullo</h1>
+          <h2>Diseñador & Desarrollador Web</h2>
+          <a href="">VER MIS TRABAJOS</a>
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <section id="about-me">
+        <div class="container">
+          <h3>Sobre mi</h3>
+          <div class="container-about-me">
+            <div class="computer">
+              <img src="./img/computadoras.png" />
+            </div>
+            <div class="text">
+              <p>
+                Mi nombre es Fernando Chullo y soy diseñador de páginas web.
+                Tengo la experiencia en desarrollo de sitios web para pequeñas y
+                medianas empresas, hasta tiendas en línea con carrito de
+                compras.
+              </p>
+              <p>
+                Me aseguro de utilizar siempre las últimas tecnologías de
+                desarrollo web: HTML5, CSS3, jQuery, Wordpress. Si necesitás una
+                página web moderna, funcional, que se adapte a todos los
+                diferentes tipos de pantalla, te aseguro que llegaste al lugar
+                correcto.
+              </p>
+              <p>
+                Podes ponerte en contacto conmigo a través de Facebook, Linkedin
+                o completando el formulario de contacto que se encuentra al
+                final de mi sitio web.
+              </p>
+              <a href="">VER MIS TRABAJOS</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="services">
+        <div>
+          <h3>Servicios</h3>
+          <div class="container-services">
+            <div class="service violet">
+              <h4>Diseño web</h4>
+              <p>
+                Páginas web atractivas y funcionales con el objetivo de
+                satisfacer todas tus expectativas. La importancia del diseño es
+                fundamental para tener éxito y atraer a tus visitantes. También
+                lo es la velocidad de carga del sitio web, deben ser rápidos
+                para que el visitante tenga la mejor experiencia
+              </p>
+
+              <img class="icon" src="./img/monitor.png" />
+              <img class="ondas" src="./img/waves.png" />
+            </div>
+            <div class="service skyblue">
+              <h4>Resposive Desing</h4>
+              <p>
+                Hoy en día las páginas web se ven en multitud de dispositivos
+                como tablets, celulares, libros electrónicos, notebooks, PCs,
+                etc. La tecnología móvil es el presente y el futuro. Para eso es
+                importante y me aseguro de que tu sitio web sea compatible con
+                todos los tamaños de pantalla de esta época moderna
+              </p>
+
+              <img class="icon" src="./img/mobile.png" />
+              <img class="ondas" src="./img/waves.png" />
+            </div>
+            <div class="service violet">
+              <h4>SEO</h4>
+              <p>
+                Al crear sitios web, tengo muy en cuenta la optimización de
+                motores de búsqueda (SEO). Llevo a cabo prácticas de SEO
+                estándar en cada página del sitio web, lo que permite que tu
+                sitio tenga una clasificación más alta en los resultados de
+                Google y asi generar mas tráfico a la web
+              </p>
+
+              <img class="icon" src="./img/seo.png" />
+              <img class="ondas" src="./img/waves.png" />
+            </div>
+            <div class="service skyblue">
+              <h4>Autoadministrable</h4>
+              <p>
+                Es importante que puedas administrar tu página web vos mismo,
+                desde cualquier lugar y en cualquier momento. Desarrollo
+                sistemas de gestión de contenido que te permiten ver los datos
+                de tu página web y editar el contenido que quieras, lo que te
+                permite mantenerte actualizado
+              </p>
+
+              <img class="icon" src="./img/wordpress.png" />
+              <img class="ondas" src="./img/waves.png" />
+            </div>
+            <div class="service violet">
+              <h4>Mantenimiento</h4>
+              <p>
+                Podés contactarme si necesitas actualizar tu página web, agregar
+                nuevas secciones o cualquier cambio que necesites. Esto es
+                perfecto para los sitios web de pequeñas empresas, donde no se
+                necesita un diseñador de páginas web a tiempo completo, sino
+                solo en distintas ocasiones
+              </p>
+
+              <img class="icon" src="./img/mante.png" />
+              <img class="ondas" src="./img/waves.png" />
+            </div>
+            <div class="service skyblue">
+              <h4>Marketing Digital</h4>
+              <p>
+                Utilizo las herramientas mas importantes como Google Adwords y
+                Google Analytics, para que tu negocio aparezca en los resultados
+                de búsqueda de Google justo cuando alguien te necesita y
+                analizar como los usuarios interactúan con tu página web
+              </p>
+
+              <img class="icon" src="./img/marketing.png" />
+              <img class="ondas" src="./img/waves.png" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="my-jobs">
+        <div class="container">
+          <h3>Mis trabajos</h3>
+          <div class="owl-carousel owl-theme">
+            <div class="item">
+              <a href="www.google.com">
+                <img src="img/sassy.jpg" />
+                <p>Sassy Kids</p>
+              </a>
+            </div>
+            <div class="item">
+              <a href="www.google.com">
+                <img src="img/sassy.jpg" />
+                <p>Sassy Kids</p>
+              </a>
+            </div>
+            <div class="item">
+              <a href="www.google.com">
+                <img src="img/sassy.jpg" />
+                <p>Sassy Kids</p>
+              </a>
+            </div>
+            <div class="item">
+              <a href="www.google.com">
+                <img src="img/sassy.jpg" />
+                <p>Sassy Kids</p>
+              </a>
+            </div>
+            <div class="item">
+              <a href="www.google.com">
+                <img src="img/sassy.jpg" />
+                <p>Sassy Kids</p>
+              </a>
+            </div>
+            <div class="item">
+              <a href="www.google.com">
+                <img src="img/sassy.jpg" />
+                <p>Sassy Kids</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="why-works">
+        <div class="container">
+          <h3>¿Por que trabajar conmigo?</h3>
+          <div class="container-work">
+            <div class="item">
+              <ul>
+                <li><img src="img/velocidad.png" /></li>
+                <li>Velocidad</li>
+                <li>
+                  Me aseguro de que tu web cargue entre 2 y 4 segundos (depende
+                  de la cantidad de contenido del sitio). Si una web tarda más
+                  de 5 segundos en cargar, el 70% de los usuarios lo cierra y no
+                  vuelve a entrar más
+                </li>
+              </ul>
+            </div>
+            <div class="item">
+              <ul>
+                <li><img src="img/mantenimiento.png" alt="" /></li>
+                <li>Mantenimiento</li>
+                <li>
+                  Mantengo una comunicación constante con mi cliente, para
+                  enseñarle a modificar contenido mediante el panel de Wordpress
+                  o para cualquier duda/cambio que quiera hacer en su sitio web
+                </li>
+              </ul>
+            </div>
+            <div class="item">
+              <ul>
+                <li><img src="img/mobile2.png" alt="" /></li>
+                <li>Mobile Priority</li>
+                <li>
+                  Cada vez mas personas navegan desde sus celulares en cualquier
+                  parte del mundo. Me aseguro de que tu web esté optimizada para
+                  cualquier tipo de dispositivo móvil
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="blog">
+        <div class="container">
+          <h3>Blog</h3>
+          <div class="container-publication">
+            <div class="publication">
+              <img src="img/blog1.jpg" />
+              <div class="content">
+                <h4>No alcanza con tener solamente una pagina web</h4>
+                <p>
+                  ¿Quieres aprender cales son las heraamientas que usan las
+                  mejores marcas para generar trafico?
+                </p>
+                <a href="">Leer mas ➝</a>
+              </div>
+            </div>
+            <div class="publication">
+              <img src="img/blog2.jpg" />
+              <div class="content">
+                <h4>¿Que es el remarketing y como utilizarlo?</h4>
+                <p>
+                  El remarketing es una de las tecnicas mas utilizadas por los
+                  mejores compañias.¿Quieres saber de que se trata?
+                </p>
+                <a href="">Leer mas ➝</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id='what-are-you-waiting'>
+          <div class="container">
+              <h3>¿Que esperas para tener tu pagina web?</h3>
+            <a href=""> CHARLEMOS</a>
+          </div>
+      </section>
+
+      <section id="contact">
+        <?php
+          if(@$_GET['i'] =='ok'){
+            ?>
+            <h3>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h3>
+            <?php   
+          }else{
+          ?>
+          
+        <div class="container">
+          <h3>Contacto</h3>
+          <form action="envio-formulario.php" method="post" enctype="multipart/form-data">
+            <input type="text" placeholder="Nombre" name="nombre" required>
+            <input type="email" placeholder="Email" name="email" required>
+            <textarea placeholder="Mensaje" name="mensaje" required></textarea>
+            <input type="submit" value="ENVIAR MENSAJE">
+          </form>
+          <?php }?>
+          <div class='contact-info'>
+            <div class='mail'>
+            <img src='img/mail-icon.png'>
+            fernando.mahiler@gmail.com
+            </div>
+            <div class='whatsapp'>
+            <img src='img/whatsapp-icon.png'>
+            +51 945373476
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer>
+          <div class='container'> 
+          <div class='network'> 
+            <a href='#'> 
+              <img src="img/facebook.png">
+            </a>
+            <a href='#'> 
+              <img src="img/github.png">
+            </a>
+            <a href='#'> 
+              <img src="img/linkedin.png">
+            </a>
+            <a href='#'> 
+              <img src="img/medium.png">
+            </a>
+          </div>
+
+          <div class="paragraph">
+            <p>© 2020 Fernando Chullo Mamani | Diseñador Web Perú. Todos los derechos reservados</p>
+   
+          </div>
+          </div>
+  </footer>
+
+    <script
+      src="https://code.jquery.com/jquery-3.4.1.min.js"
+      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+      crossorigin="anonymous"
+    ></script>
+    <script src="owl/owl.carousel.min.js"></script>
+
+    <script>
+      $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        dots: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: true
+          },
+          600: {
+            items: 3,
+            nav: false
+          },
+          1000: {
+            items: 4,
+            nav: true,
+            loop: false
+          }
+        }
+      });
+    </script>
+    <script>
+      $(document).ready(function(){
+        $(window).scroll(function(){
+          scroll = $(window).scrollTop();
+          if (scroll > 100){
+            $(".menu").css({"position":"fixed"})
+            $(".menu").css({"width":"100%"})
+            $(".menu").css({"top":"0"})
+            $(".menu").css({"background":"#fff"})
+            $(".menu a").css({"color":"#2f2f2f"})
+            $(".logo").css({"color":"#2f2f2f"})
+            $(".menu").css({"box-shadow":"rgba(0,0,0,0.22) 6px 1px 1px"})
+            $(".menu").css({"z-index":"100"})
+          }else{
+            $(".menu").css({"position":"relative"})
+            $(".menu").css({"background":"transparent"})
+            $(".menu").css({"box-shadow":"0 0 0"})
+            $(".menu a").css({"color":"#fff"})
+            $(".logo").css({"color":"#fff"})
+          }
+        })
+        $(".menu-icon").click(function(){
+          $("header nav").slideToggle();
+        })
+      })
+    </script>
+  </body>
+</html>
